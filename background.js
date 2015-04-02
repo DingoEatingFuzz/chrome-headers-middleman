57f31d69de964227f574f9bee255c37fe8c499d7
Michael Lange <dingoeatingfuzz@gmail.com>
2015-04-02 03:36:02 -0700

    Minimum functionality achieved!

--- background.js
+++ background.js
@@ -1,1 +1,41 @@
-console.log('yep')
+var rules = []
+refresh()
+
+chrome.storage.onChanged.addListener(function() {
+  refresh()
+})
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    if (details.type === 'main_frame') {
+      rules.forEach(function(rule) {
+        if (new RegExp(rule.pattern).test(details.url)) {
+          rule.headers.forEach(function(header) {
+            if (header.value === null) {
+              // delete
+            } else {
+              // try to find
+              // else push
+              details.requestHeaders.push({
+                name: header.name,
+                value: header.value
+              })
+            }
+          })
+        }
+      });
+    }
+    return { requestHeaders: details.requestHeaders }
+  },
+  {
+    urls: [ '<all_urls>' ]
+  },
+  [ 'blocking', 'requestHeaders' ]
+)
+
+function refresh() {
+  chrome.storage.sync.get(null, function(d) {
+    console.log('updating!', d)
+    rules = d.rules
+  })
+}
