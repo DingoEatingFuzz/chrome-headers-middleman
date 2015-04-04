161691f5208a2a8f438c7835fac0bfb9b021b9d1
Michael Lange <dingoeatingfuzz@gmail.com>
2015-04-04 14:17:08 -0700

    Sets up the publishing pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var browserify = require('browserify')
 var babelify = require('babelify')
 var through = require('through2')
 var mv = require('gulp-rename')
+var exec = require('child_process').exec
 
 var jsFiles = 'src/**/*.js'
 var staticFiles = [
@@ -10,6 +11,7 @@ var staticFiles = [
   'background.js',
   'options.html',
   'styles.css',
+  'images/**/*'
 ]
 
 var reactFiles = [
@@ -18,26 +20,10 @@ var reactFiles = [
 
 gulp.task('default', [ 'static', 'scripts' ])
 
-gulp.task('scripts', function () {
-  return gulp.src('src/index.js')
-    .pipe(through.obj(function (file, enc, next) {
-      browserify(file.path, { debug: true })
-        .transform(babelify)
-        .bundle(function (err, res) {
-          if (err) return next(err)
-          file.contents = res
-          next(null, file)
-        })
-    }))
-    .on('error', function (error) {
-      console.log(error.stack)
-      this.emit('end')
-    })
-    .pipe(mv('app.js'))
-    .pipe(gulp.dest('dist'))
-})
+gulp.task('scripts', scriptsBuilder(true))
+gulp.task('productionScripts', scriptsBuilder(false))
 
-gulp.task('static', function () {
+gulp.task('static', function() {
   gulp.src(reactFiles, { base: './bower_components/react' })
     .pipe(gulp.dest('dist'))
   return gulp.src(staticFiles, { base: './' })
@@ -48,3 +34,32 @@ gulp.task('watch', [ 'default' ], function() {
   gulp.watch(jsFiles, [ 'scripts' ])
   gulp.watch(staticFiles, [ 'static' ])
 })
+
+gulp.task('publish', [ 'static', 'productionScripts' ], function() {
+  exec('zip headers-middleman.zip dist/*', function(err) {
+    if (err !== null) {
+      throw err;
+    }
+  })
+})
+
+function scriptsBuilder(debug) {
+  return function() {
+    return gulp.src('src/index.js')
+      .pipe(through.obj(function (file, enc, next) {
+        browserify(file.path, { debug: debug })
+          .transform(babelify)
+          .bundle(function (err, res) {
+            if (err) return next(err)
+            file.contents = res
+            next(null, file)
+          })
+      }))
+      .on('error', function (error) {
+        console.log(error.stack)
+        this.emit('end')
+      })
+      .pipe(mv('app.js'))
+      .pipe(gulp.dest('dist'))
+  }
+}
